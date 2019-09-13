28c7ab5e696ac0dce174b864843ccb17a93983dd
igor <koiv.igor@gmail.com>
2019-09-13 15:55:40 +0300

    Bork testcase

---
+++ src/test/java/ru/org/BorkTehnoparkTest.java
@@ -0,0 +1,60 @@
+package ru.org;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class BorkTehnoparkTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        String url = "";
+        HttpURLConnection huc = null;
+        int respCode = 200;
+        String homePage = "https://www.bork.ru/about/awards/";
+        driver.manage().window().maximize();
+        driver.get(homePage);
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        for (WebElement link : links) {
+            url = link.getAttribute("href");
+            System.out.println(url);
+            if (url == null || url.isEmpty()) {
+                System.out.println("URL is empty");
+                continue;
+            }
+            if (!url.startsWith(homePage)) {
+                System.out.println("URL belongs to another domain, skipping it.");
+                continue;
+            }
+            try {
+                huc = (HttpURLConnection) (new URL(url).openConnection());
+
+                huc.setRequestMethod("HEAD");
+                huc.connect();
+                respCode = huc.getResponseCode();
+
+                if (respCode >= 400) {
+                    System.out.println(url + " is a broken link");
+                } else {
+                    System.out.println(url + " is a valid link");
+                }
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        driver.quit();
+    }
+}
